3593a4461ed7bfedd14b9e0de48b0bb27c27624f
Franky <gfferike@gmail.com>
2017-12-14 10:21:22 +0100

    Print Usage

---
+++ PrintUsage/src/PrintUsage.java
@@ -0,0 +1,11 @@
+public class PrintUsage {
+  public static void main(String[] args) {
+    System.out.println("Command Line Todo application");
+    System.out.println("=============================\n");
+    System.out.println("Command line arguments:");
+    System.out.println(" -l   Lists all the tasks");
+    System.out.println(" -a   Adds a new task");
+    System.out.println(" -r   Removes an task");
+    System.out.println(" -c   Completes an task");
+  }
+}
